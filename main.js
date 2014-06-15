fbb1702d5736e0f89a7bbee3118bd3bdc7d335f1
Jason <bjason@gmail.com>
2014-06-15 00:44:32 -0700

    Trying to add a second box.

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 $(function(){
     var colorPicker = $("#color-picker");
+
     colorPicker.change(function(){
         console.log(colorPicker.val());
-        var cssColor = generateGreyCSS();
-        console.log(cssColor);
-        $("#colorId").css({"background-color": cssColor});
     });
 
     var _generateGreyColor = function(){
         var ii = Math.floor(Math.random() * 32);
-
         var rgb = ii*8;
-
         return rgb;
     };
 
@@ -19,4 +15,24 @@ $(function(){
         var rgb = _generateGreyColor();
         return "rgb(" + rgb + "," + rgb + "," + rgb +")";
     };
+
+    function hexToRGBA(h){
+        return "rgb(" + hexToR(h) + "," + hexToG(h) + "," + hexToB(h) + ")";
+    };
+
+    function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
+    function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
+    function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
+    function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
+
+
+    // Setup css color
+    var cssColor = generateGreyCSS();
+    $("#colorId").css({"background-color": cssColor});
+
+    // Generate CSS color:
+    var targetColor = generateGreyCSS();
+
+
+
 });
