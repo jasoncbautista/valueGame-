4542b29d4357679d9016cee8f3c2757a642ff3f8
Jason <bjason@gmail.com>
2014-07-05 08:52:19 -0700

    Loadig iframes.

--- many.js
+++ many.js
@@ -2,4 +2,11 @@
 $(function(){
     console.log('hi');
 
+    for (var ii = 0; ii < 8; ii++){
+
+        var frame = $("<div> <iframe src='/index.html'></iframe></div>");
+        $("#iframeContainer").append(frame);
+    }
+
+
 });
